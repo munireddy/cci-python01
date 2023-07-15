860d65a53843bd33b5938121d6a6fea9df49353a
munireddy <munisekhar.reddy@gmail.com>
2023-07-15 12:01:05 -0400

    Update findFirstIndexOfSubstringInaString.py

--- findFirstIndexOfSubstringInaString.py
+++ findFirstIndexOfSubstringInaString.py
@@ -9,7 +9,18 @@ def findIndex(str1, str2):
       return i
   return -1
 
+def isDuplicateElementPresentInArray(arr1):
+  for i in range(0, len(arr1) -1 ):
+    if arr1.count(arr1[i]) >1 :
+      return 1
+  return -1    
+
 if __name__ == "__main__":
   str1 = "sadpeopletalksadideasinsadmoods"
   str2 = "sad"
   print(findIndex(str1, str2))
+ 
+  #Second fucntion
+  a = [1,2,3,4,5,6,1,3,3]
+  print(isDuplicateElementPresentInArray(a))
+  
