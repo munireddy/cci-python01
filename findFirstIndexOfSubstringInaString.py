c82875d03986faa6b73dd3e894edf3b3d256cf87
munireddy <munisekhar.reddy@gmail.com>
2023-07-15 11:42:41 -0400

    Update findFirstIndexOfSubstringInaString.py

--- findFirstIndexOfSubstringInaString.py
+++ findFirstIndexOfSubstringInaString.py
@@ -1,11 +1,13 @@
 
 def findIndex(str1, str2):
+  if len(str2) > len(str1):
+    return -1
   for i in range(0, len(str1) - len(str2)+1):
     if str1[i:i+len(str2)] == str2:
       return i
-  return '-1'
+  return -1
 
 if __name__ == "__main__":
   str1 = "sadpeopletalksadideasinsadmoods"
-  str2 = "mun"
+  str2 = "munfddfdssssssssssssssssssssssssssss"
   print(findIndex(str1, str2))
